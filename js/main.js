9bfcf022b6d8999b93c21238809505374a99a63c
sammyfares <sammyfares2003@gmail.com>
2022-09-19 18:22:59 -0400

    add randmoize function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /*----- constants -----*/ 
-const arrNum = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-const arrSymbol = ['Hearts', 'Spades', 'Clubs', 'Diamonds']
+const arrNum = ['A', '02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K']
+const arrSymbol = ['h', 's', 'c', 'd']
 
 
 
@@ -8,6 +8,7 @@ const arrSymbol = ['Hearts', 'Spades', 'Clubs', 'Diamonds']
 let deckOfCards = []
 let winner 
 let valueOfUniqueCards
+let newDeck = []
 
 
 
@@ -29,6 +30,8 @@ resetBtnEl = document.getElementById('reset')
 
 /*----- event listeners -----*/ 
 
+hitBtnEl.addEventListener('click', randomizeDeck)
+
 
 
 
@@ -41,7 +44,9 @@ resetBtnEl = document.getElementById('reset')
 // and then we put it inside an object with value and symbol 
 // and then we set deck of card to empty array and we push it inside of it
 function init() {
-
+    makeDeck()
+    // console.log(deckOfCards)
+    randomizeDeck()
 }
 
 function makeDeck() {
@@ -58,12 +63,38 @@ function makeDeck() {
         }
     }
 }
-// console.log(makeDeck())
-// console.log(deckOfCards[10].value)
+
+function randomizeDeck() {
+    // first thing we are getting a random index 
+    // then we store that random index in removecard 
+    // then we remove that index from deck of cards and we add it to the new deck
+    let randomCard = Math.floor(Math.random() * deckOfCards.length)
+    let removeCard = deckOfCards.splice(randomCard, 1)
+    if(newDeck.length < 52) {
+        newDeck.push(removeCard)
+    } else {
+        return 
+    }
+    
+    renderHitTwoCards()
+    // console.log(newDeck)
+    // console.log(deckOfCards)
+}
+
+function renderHitTwoCards() { 
+    for(let i = 0; i < newDeck.length; i++) {
+        for(let j = 0; j < newDeck.length; j++) {
+        let cardValue = `${newDeck[i][j].symbol}${newDeck[i][j].value}`
+        console.log(cardValue)
+        console.log(newDeck)
+        }
+    }
+    
+}
 
 
 
 
 
-
+init()
 
